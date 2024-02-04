23a9fceefba8ee3bfb6bb6df954fe3ee52688ed1
aliakbarzohour <aliakbar.zohour@gmail.com>
2024-02-04 15:41:04 +0330

    new config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import image from "@astrojs/image"
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
